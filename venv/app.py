a6003ab0e83a58f42d488e41a02e03c05e75737e
Adkhey74 <108014175+Adkhey74@users.noreply.github.com>
2023-12-20 11:07:29 +0100

    app.py

---
+++ venv/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Votre logique de traitement peut être ajoutée ici
+    # Par exemple, rendre un modèle avec render_template
+    return render_template('home.html',active_page='Home')
+
+
+@app.route('/projet.html')
+def projet():
+    return render_template('projet.html', active_page='Projet')
+
+@app.route('/cv.html')
+def cv():
+    return render_template('cv.html', active_page='CV')
+
+
+@app.route('/download_pdf')
+def download_pdf():
+    pdf_path = 'Static/media/CV.pdf'  # Remplacez cela par le chemin réel de votre fichier PDF
+    return send_file(pdf_path, as_attachment=True)
+if __name__ == '__main__':
+    app.run(debug=True)
